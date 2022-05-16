54ff0c728c7f1ef1857ccd89b87af450f8082929
minasweb <102960172+minasweb@users.noreply.github.com>
2022-05-16 11:46:40 +0200

    popup gallery

--- _site/scripts.js
+++ _site/scripts.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     AOS.init();
     
 });
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+   download: false
+   });
+   
+
+   lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+   download: false
+   });
+   
